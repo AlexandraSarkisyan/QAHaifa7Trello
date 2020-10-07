a47832c44fae08921a339b15fdc5175e8f775366
AlexandraSarkisyan <alexandra13sark@gmail.com>
2020-10-07 22:38:09 +0300

    adding changes

--- src/Tests/TestBase.java
+++ src/Tests/TestBase.java
@@ -23,37 +23,10 @@ public class TestBase {
         driver.get("https://trello.com/");
         waitUntilElementIsPresent(By.xpath("//a[@class='btn btn-sm btn-link text-white']"),10);
     }
-    public void waitUntilElementIsClickable(By locator, int time) {
-        try {
-            new WebDriverWait(driver,time).until(ExpectedConditions.elementToBeClickable(locator));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
     public void waitUntilElementIsPresent(By locator, int time) {
         try {
             new WebDriverWait(driver,time).until(ExpectedConditions.presenceOfElementLocated(locator));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public void waitUntilElementIsInvisible(By locator, int time) {
-        try {
-            new WebDriverWait(driver,time).until(ExpectedConditions.invisibilityOfElementLocated(locator));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public void waitUntilElementsAreVisible(By locator, int time){
-        try {
-            new WebDriverWait(driver,time).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public void waitUntilElementIsVisible(By locator, int time) {
-        try {
-            new WebDriverWait(driver,time).until(ExpectedConditions.visibilityOfElementLocated(locator));
         } catch (Exception e) {
             e.printStackTrace();
         }
